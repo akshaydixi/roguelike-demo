d6ff7a8bd4283d03babf584c78dda58d55502cd8
Akshay Dixit <akshaydixi@gmail.com>
2013-09-24 19:36:59 +0530

    added mixins

--- Javascript/assets/entity.js
+++ Javascript/assets/entity.js
@@ -4,10 +4,32 @@ Game.Entity = function(properties){
     this._name = properties['name'] || '';
     this._x = properties['x'] || 0;
     this._y = properties['y'] || 0;
+    
+    this.attachedMixins = {};
+    var mixins = properties['mixins'] || [];
+    for (var i = 0; i < mixins.length; i++) {
+        for( var key in mixins[i]){
+            if (key != 'init' && key != 'name' && !this.hasOwnProperty(key)) {
+                this[key] = mixins[i][key];
+            }
+        }
+        this._attachedMixins[mixins[i].name] = true;
+        if(mixins[i].init){
+            mixins[i].init.call(this,properties);
+        }
+    }
 }
 
 Game.Entity.extend(Game.Glyph);
 
+Game.Entity.prototype.hasMixin = function(obj){
+        if ( typeof(obj) == 'object'){
+            return this._attachedMixins[obj.name];
+        }
+        else {
+            return this._attachedMixins[name];
+        }
+}
 Game.Entity.prototype.setName = function(name) {
     this._name = name;
 }
