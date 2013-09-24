bec9501b5f403e2e83684436501f86c3e222c017
Akshay Dixit <akshaydixi@gmail.com>
2013-09-24 16:45:33 +0530

    added clojure project files

---
+++ Clojure/project.clj
@@ -0,0 +1,9 @@
+(defproject roguelike "0.1.0-SNAPSHOT"
+  :description "A test roguelike game"
+  :url "http://pokecoder.co/lab/roguelike"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojure-lanterna "0.9.0"]]
+  :main roguelike.core
+)
---
+++ Clojure/test/roguelike/core_test.clj
@@ -0,0 +1,7 @@
+(ns roguelike.core-test
+  (:require [clojure.test :refer :all]
+            [roguelike.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
